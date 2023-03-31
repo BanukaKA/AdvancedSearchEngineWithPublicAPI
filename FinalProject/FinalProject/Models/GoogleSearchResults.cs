8915c216828003dbfa75d3d3f302512a2bef90f6
Banuka Ambegoda <banukaambegoda@gmail.com>
2023-03-30 20:39:51 -0400

    Changes

---
+++ FinalProject/FinalProject/Models/GoogleSearchResults.cs
@@ -0,0 +1,11 @@
+﻿using FinalProject.Models;
+
+namespace FinalProject.Models
+{
+
+    public class GoogleSearchResults
+    {
+        public Result[] Items { get; set; }
+
+    }
+}
---
+++ FinalProject/FinalProject/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿using FinalProject.Models;
+
+namespace FinalProject.Models
+{
+
+    public class Result
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Snippet { get; set; }
+    }
+}
